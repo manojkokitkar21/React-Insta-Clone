a476d078aca6c3131158498c13eb48a30eea96a4
manojkokitkar21 <manojkokitkar21@gmail.com>
2023-03-29 15:06:06 +0530

    Added Landing page and folder structure

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import AppRouter from './routers/AppRouters';
+function App() {
+  return (
+    <div className="App-container">
+      <AppRouter/>
+    </div>
+  );
+}
+
+export default App;
